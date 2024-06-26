68ed1d0b067429da23597599533508cd03d65270
Dieg0407 <diego.pastor.0407@gmail.com>
2024-06-25 21:56:40 -0500

    chore: added logging configuration

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 
 dependencies {
     testImplementation(libs.junit.jupiter)
+    testImplementation("org.assertj:assertj-core:3.25.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     api(libs.commons.math3)
@@ -22,4 +23,14 @@ java {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+
+    testLogging {
+        showStandardStreams = true
+        events("passed", "failed", "skipped")
+
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.SHORT
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
 }
